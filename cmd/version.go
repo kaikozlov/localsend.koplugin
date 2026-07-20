89b02746e1bc581cf8caf0f51b309ff24372512e
Kai Kozlov <37962720+kaikozlov@users.noreply.github.com>
2026-07-20 14:26:33 -0500

    chore: bump version to v1.4.3

--- cmd/version.go
+++ cmd/version.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-var version = "v1.4.2"
+var version = "v1.4.3"
 
 // buildArchTag is injected at build time via -ldflags "-X localsend-cli/cmd.buildArchTag=<arch>"
 // by the release builds (justfile `release` recipe, .github/workflows/koplugin.yaml) using the same vocabulary
--- lua/_meta.lua
+++ lua/_meta.lua
@@ -3,5 +3,5 @@ return {
     name = "localsend",
     fullname = _("LocalSend"),
     description = _([[Send and receive files using LocalSend protocol.]]),
-    version = "v1.4.2",
+    version = "v1.4.3",
 }
